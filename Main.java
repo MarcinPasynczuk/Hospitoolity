f8690bdbd8a1c56961ed5fa63ec1a44645b0b5e7
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-12-13 11:29:20 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+ Test test = new Test("19/02/1992", "10", "9", "ok");
+//        test.saveData();
+        test.reader();
+// Test test2 = new Test("19/02/1900", "8", "4", "not ok");
+//        test2.saveData();
+//        test2.reader();
+    }
+}
